bb9922bfb2fcca710cde8d24a9b45b625fe5ca9f
kongxiao0532 <kongxiao0532@163.com>
2017-08-05 09:27:00 +0800

    在application中增加了一些变量

--- app/src/main/java/com/courseratingsystem/app/application/MyCourseApplication.java
+++ app/src/main/java/com/courseratingsystem/app/application/MyCourseApplication.java
@@ -14,6 +14,9 @@ public class MyCourseApplication extends Application {
 
     public static final String SERVER_URL = "http://192.168.137.1:8080/CourseRatingSystem";
     public OkHttpClient okHttpClient;
+    public Integer userId;
+    public String username;
+    public String nickname;
 
     public OkHttpClient getOkHttpClient() {
         return okHttpClient;
@@ -24,5 +27,7 @@ public class MyCourseApplication extends Application {
         super.onCreate();
         x.Ext.init(this);
         okHttpClient = new OkHttpClient();
+        userId = null;
+        username = nickname = null;
     }
 }
