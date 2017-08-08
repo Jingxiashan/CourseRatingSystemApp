782b835f18e0d190cd20fd07ed6c0f387fa7459e
Jingxiashan <847094506@qq.com>
2017-08-08 16:12:11 +0800

    完成CommentActivity/CommentPopupActivity/addCommentActivity.xml

---
+++ app/src/main/java/com/courseratingsystem/app/activity/CommentPopupActivity.java
@@ -0,0 +1,15 @@
+package com.courseratingsystem.app.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.courseratingsystem.app.R;
+
+public class CommentPopupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_popup);
+    }
+}
