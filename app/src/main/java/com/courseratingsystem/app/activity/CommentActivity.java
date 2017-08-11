9defe8125438b99891fdc86e569a23c490589e99
kongxiao0532 <kongxiao0532@163.com>
2017-08-11 12:07:19 +0800

    commentactivity 联网完成

--- app/src/main/java/com/courseratingsystem/app/activity/CommentActivity.java
+++ app/src/main/java/com/courseratingsystem/app/activity/CommentActivity.java
@@ -9,11 +9,10 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,72 +22,213 @@ import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.courseratingsystem.app.R;
-import com.courseratingsystem.app.view.CommentListView;
+import com.courseratingsystem.app.application.MyCourseApplication;
 import com.courseratingsystem.app.view.CourseListSwipeRefreshView;
 import com.courseratingsystem.app.vo.Comment;
-import com.courseratingsystem.app.vo.Course;
-import com.courseratingsystem.app.vo.Teacher;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 @ContentView(R.layout.activity_comment)
 public class CommentActivity extends AppCompatActivity {
     public static final String COURSE_INFO = "course_info";
-
+    public static final String BUNDLE_TYPE = "type";
+    public static final String BUNDLE_ID = "id";
+    public static final String BUNDLE_NAME = "name";
+    private static final String COURSE_COMMENT_URL = "/getCommentListByCourseId?courseId=";
+    private static final String TEACHER_COMMENT_URL = "/getCommentListByTeacherId?teacherId=";
+    private static final String PARAM_CURRENT_PAGE = "&currentPage=";
+    private static final int FAILED = 0;
+    private static final int LOAD_RESET = 1;
+    private static final int LOAD_APPEND = 2;
     private static int TYPE_REFRESH = 0;
     private static int TYPE_LOADMORE = 1;
-    private static Handler loadDataHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            //TODO：根据sortType和msg.what加载新数据
-            if (msg.what == TYPE_REFRESH) {
-                Log.e("LISTSCROLL", "刷新");
-            } else if (msg.what == TYPE_LOADMORE) {
-                Log.e("LISTSCROLL", "加载新数据");
-
-            }
-        }
-    };
-
     AnimatorSet hideElementAnimatorSet;//这是隐藏头尾元素使用的动画
     AnimatorSet showElementAnimatorSet;//这是显示头尾元素使用的动画
-
+    @ViewInject(R.id.activity_comment_layout)
+    private RelativeLayout mRelativeLayout;
     @ViewInject(R.id.activity_comment_layout_courseheaderinfo)
-    LinearLayout courseHeaderInfor;
+    private LinearLayout mCourseHeaderInfo;
     @ViewInject(R.id.activity_comment_listview_allcomments)
-    private CommentListView allcomments_listView;
+    private ListView mAllCommentsList;
     @ViewInject(R.id.activity_comment_text_courseName)
-    private TextView courseName;
+    private TextView mCourseName;
     @ViewInject(R.id.activity_comment_linear_teacherlayout)
-    private LinearLayout teacherLayout;
+    private LinearLayout mTeacherLayout;
     @ViewInject(R.id.activity_commentlist_refresh)
-    private CourseListSwipeRefreshView commentSwipRefresh;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        x.view().inject(this);
+    private CourseListSwipeRefreshView mCommentSwipeRefresh;
+    private View mFooterView;
+    private CommentsAdapter commentsAdapter;
+    private final Handler getCommentHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case LOAD_RESET:
+                    List<Comment> commentList = (List<Comment>) msg.obj;
+                    commentsAdapter = new CommentsAdapter(CommentActivity.this, commentList);
+                    mAllCommentsList.setAdapter(commentsAdapter);
+                    break;
+                case LOAD_APPEND:
+                    List<Comment> newCommentList = (List<Comment>) msg.obj;
+                    List<Comment> oldCommentList = commentsAdapter.getCommentList();
+                    oldCommentList.addAll(newCommentList);
+                    commentsAdapter.notifyDataSetChanged();
+                    break;
+                case FAILED:
+                    break;
+            }
+            mCommentSwipeRefresh.setRefreshing(false);
+            return false;
+        }
+    });
+    private int totalPage;
+    private int currentMaxLoadedPage;
+    private CommentType commentType;
+    private int id;
+    private String name;
+    ;
 
+    private void initData() {
+        currentMaxLoadedPage = 0;
+        loadData(1);
+    }
+
+    private void loadMore() {
+        if (currentMaxLoadedPage < totalPage) {
+            if (mAllCommentsList.getFooterViewsCount() > 1) {
+                mAllCommentsList.removeFooterView(mFooterView);
+            }
+            loadData(currentMaxLoadedPage + 1);
+        } else {
+            //已经到达最后一页
+            mCommentSwipeRefresh.setLoadingMore(false);
+            if (mAllCommentsList.getFooterViewsCount() == 0) {
+                //添加一个已经到最低 的 footerview
+                mAllCommentsList.addFooterView(mFooterView);
+            }
+        }
+    }
+
+    private void loadData(final int page) {
+        mCommentSwipeRefresh.setRefreshing(true);
+        String url = MyCourseApplication.SERVER_URL;
+        switch (commentType) {
+            case COURSE_COMMENTS:
+                url += COURSE_COMMENT_URL + id;
+                break;
+            case TEACHER_COMMENTS:
+                url += TEACHER_COMMENT_URL + id;
+                break;
+        }
+        url += PARAM_CURRENT_PAGE + page;
+        MyCourseApplication application = (MyCourseApplication) getApplication();
+        OkHttpClient okHttpClient = application.getOkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            Message msg = new Message();
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                msg.what = FAILED;
+                msg.obj = e.toString();
+                getCommentHandler.sendMessage(msg);
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.body() != null) {
+                    String responseBody = response.body().string();
+                    try {
+                        JSONObject responseJson = new JSONObject(responseBody);
+                        int statusCode = responseJson.getInt(MyCourseApplication.JSON_RESULT_CODE);
+                        if (statusCode == MyCourseApplication.JSON_RESULT_CODE_200) {
+                            //成功，处理内容
+                            JSONObject resultJson = responseJson.getJSONObject(MyCourseApplication.JSON_RESULT);
+
+                            totalPage = resultJson.getInt("totalPage");
+                            currentMaxLoadedPage = resultJson.getInt("currentPage");
+                            JSONArray commentJsonList = resultJson.getJSONArray("commentList");
+                            msg.obj = Comment.parseJsonList(commentJsonList);
+                            if (page == 1) {
+                                //如果加载了第一页，就重设整个listview的list
+                                msg.what = LOAD_RESET;
+                            } else {
+                                //如果加载了其他页，就在原来的list基础上追加新的list
+                                msg.what = LOAD_APPEND;
+                            }
+                        } else {
+                            msg.what = FAILED;
+                            msg.obj = responseJson.getString(MyCourseApplication.JSON_REASON);
+                        }
+                        getCommentHandler.sendMessage(msg);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void initView() {
+        mFooterView = View.inflate(this, R.layout.footer_fragment_discover_hot_courses_list, null);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         View headerView = new View(this);
         int w = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
         int h = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
-        courseHeaderInfor.measure(w, h);
-        headerView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) courseHeaderInfor.getMeasuredHeight()));
+        mCourseHeaderInfo.measure(w, h);
+        headerView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) mCourseHeaderInfo.getMeasuredHeight()));
         headerView.setBackgroundColor(Color.parseColor("#00000000"));
-        allcomments_listView.addHeaderView(headerView);
-        allcomments_listView.setOnSwipeListener(new CommentListView.OnSwipeListener() {
+        mAllCommentsList.addHeaderView(headerView);
+
+
+        mCourseName.setText(name);
+
+
+        actionBar.setTitle("全部评论");
+
+        //下拉刷新
+        mCommentSwipeRefresh.setProgressViewOffset(true, 0, getResources().getDimensionPixelOffset(R.dimen.activity_index_search_height));
+        mCommentSwipeRefresh.setColorSchemeColors(Color.GRAY, Color.BLACK, getResources()
+                .getColor(R.color.teacher_blue_light));
+        mCommentSwipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onSwipe(CommentListView.SwipeDirect swipeDirect) {
+            public void onRefresh() {
+                initData();
+            }
+        });
+        mCommentSwipeRefresh.setOnLoadMoreListener(new CourseListSwipeRefreshView.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                loadMore();
+            }
+        });
+        mCommentSwipeRefresh.setOnSwipeListener(new CourseListSwipeRefreshView.OnSwipeListener() {
+            @Override
+            public void onSwipe(CourseListSwipeRefreshView.SwipeDirect swipeDirect) {
                 switch (swipeDirect) {
                     case UP:
                         startHiddingToolbar();
@@ -97,126 +237,23 @@ public class CommentActivity extends AppCompatActivity {
                         startShowingToolbar();
                         break;
                 }
-            }
-        });
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
-        //设置标题课程信息
-        if (getIntent().getSerializableExtra(COURSE_INFO) == Course.class) {
-            final Course course = (Course) getIntent().getSerializableExtra(COURSE_INFO);
-            courseName.setText(course.getCourseName());
-
-        }
-        if (getIntent().getSerializableExtra(COURSE_INFO) == Teacher.class) {
-            final Teacher teacher = (Teacher) getIntent().getSerializableExtra(COURSE_INFO);
-            courseName.setText(teacher.getTeachername());
-
-        }
-
-        //mCourseName.setText(course.getCourseName());
-        courseName.setText("Course_name");
-        //courseRecRatingBar.setNumStars((int)course.getRecommendationScore());
-
-
-//        for(int i=0;i<course.getTeacherList().size();i++){
-//            TextView teacherText = new TextView(this);
-//            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-//            layoutParams.setMargins(10, 10, 10,10);
-//            teacherText.setBackground(getDrawable(R.drawable.activity_comment_teacher_button_bg));
-//            teacherText.setGravity(Gravity.CENTER);
-//            teacherText.setLayoutParams(layoutParams);
-//            teacherText.setTextSize(15);
-//            teacherText.setText(course.getTeacherList().get(i).toString());
-//            teacherText.setHeight(60);
-//            teacherText.setWidth(160);
-//            teacherText.setTextColor(getResources().getColor(R.color.teacher_blue_light));
-//            teacherLayout.addView(teacherText);
-//
-//            teacherText.setOnClickListener(new View.OnClickListener() {
-//                //TODO 点击后弹出对应教师popupwindow
-//                @Override
-//                public void onClick(View v) {
-//
-//                }
-//            });
-//
-//
-//        }
-
-        List<Comment> commentList = new ArrayList<>();
-        Comment tcomment = new Comment();
-        for (int i = 0; i < 20; i++) {
-            tcomment.setTimestamp("1997-2-26");
-            tcomment.setNickname("井下山");
-            tcomment.setRecstar(4);
-            tcomment.setContent("hhhhhhhhhhh");
-            tcomment.setLikecount(100);
-            commentList.add(tcomment);
-
-        }
-
-        //测试
-        TextView teacherText = new TextView(this);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(20, 20, 20, 20);
-        teacherText.setBackground(getDrawable(R.drawable.activity_comment_teacher_button_bg));
-        teacherText.setGravity(Gravity.CENTER);
-        teacherText.setLayoutParams(layoutParams);
-        teacherText.setTextSize(15);
-        teacherText.setText("孔啸");
-        teacherText.setHeight(60);
-        teacherText.setWidth(180);
-        teacherText.setTextColor(getResources().getColor(R.color.teacher_blue_light));
-        teacherLayout.addView(teacherText);
-
-        teacherText.setOnClickListener(new View.OnClickListener() {
-            //TODO 点击后弹出对应教师popupwindow
-            @Override
-            public void onClick(View v) {
 
             }
         });
+    }
 
-
-        //TODO:得到commentlist
-        actionBar.setTitle("课程评论");
-        CommentsAdapter commentsAdapter = new CommentsAdapter(CommentActivity.this, commentList);
-        allcomments_listView.setAdapter(commentsAdapter);
-
-        //下拉刷新
-        commentSwipRefresh.setProgressViewOffset(true, 0, getResources().getDimensionPixelOffset(R.dimen.activity_index_search_height));
-        commentSwipRefresh.setColorSchemeColors(Color.GRAY, Color.BLACK, getResources()
-                .getColor(R.color.teacher_blue_light));
-        commentSwipRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                Message msg = new Message();
-                msg.what = TYPE_REFRESH;
-                loadDataHandler.sendMessage(msg);
-            }
-        });
-        commentSwipRefresh.setOnLoadMoreListener(new CourseListSwipeRefreshView.OnLoadMoreListener() {
-            @Override
-            public void onLoadMore() {
-                //TODO:加载更多
-            }
-        });
-        commentSwipRefresh.setOnSwipeListener(new CourseListSwipeRefreshView.OnSwipeListener() {
-            @Override
-            public void onSwipe(CourseListSwipeRefreshView.SwipeDirect swipeDirect) {
-                switch (swipeDirect) {
-                    case UP:
-
-                        break;
-                    case DOWN:
-
-                        break;
-                }
-
-            }
-        });
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        x.view().inject(this);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle == null) finish();
+        commentType = (CommentType) bundle.get(BUNDLE_TYPE);
+        id = bundle.getInt(BUNDLE_ID);
+        name = bundle.getString(BUNDLE_NAME);
+        initData();
+        initView();
     }
 
     @Override
@@ -238,7 +275,7 @@ public class CommentActivity extends AppCompatActivity {
             //如果这个动画已经在运行了，就不管它
         } else {
             hideElementAnimatorSet = new AnimatorSet();
-            ObjectAnimator headerAnimator = ObjectAnimator.ofFloat(courseHeaderInfor, "translationY", courseHeaderInfor.getTranslationY(), -courseHeaderInfor.getHeight());//将ToolBar隐藏到上面
+            ObjectAnimator headerAnimator = ObjectAnimator.ofFloat(mCourseHeaderInfo, "translationY", mCourseHeaderInfo.getTranslationY(), -mCourseHeaderInfo.getHeight());//将ToolBar隐藏到上面
             ArrayList<Animator> animators = new ArrayList<>();
             animators.add(headerAnimator);
             hideElementAnimatorSet.setDuration(200);
@@ -257,7 +294,7 @@ public class CommentActivity extends AppCompatActivity {
         } else {
             showElementAnimatorSet = new AnimatorSet();
             //下面两句是将头尾元素放回初始位置。
-            ObjectAnimator headerAnimator = ObjectAnimator.ofFloat(courseHeaderInfor, "translationY", courseHeaderInfor.getTranslationY(), 0f);
+            ObjectAnimator headerAnimator = ObjectAnimator.ofFloat(mCourseHeaderInfo, "translationY", mCourseHeaderInfo.getTranslationY(), 0f);
             ArrayList<Animator> animators = new ArrayList<>();
             animators.add(headerAnimator);
             showElementAnimatorSet.setDuration(300);
@@ -266,6 +303,7 @@ public class CommentActivity extends AppCompatActivity {
         }
     }
 
+    public static enum CommentType {COURSE_COMMENTS, TEACHER_COMMENTS}
 
     private static class CommentsViewHolder {
         ImageView avatar;
@@ -281,6 +319,10 @@ public class CommentActivity extends AppCompatActivity {
         public CommentsAdapter(Context context, List<Comment> commentList) {
             inflater = LayoutInflater.from(context);
             this.commentList = commentList;
+        }
+
+        public List<Comment> getCommentList() {
+            return commentList;
         }
 
         public void setCommentList(List<Comment> commentList) {
